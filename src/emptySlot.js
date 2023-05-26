a9580c2456ef7fb7a2caa45c243f428832246b37
Diego Velasquez <100886672+da-velasquezgomez@users.noreply.github.com>
2023-05-25 21:19:26 -0700

    Completed

---
+++ src/emptySlot.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { useState } from "react";
+import MoleContainer from "./MoleContainer";
+
+// App defined
+function App() {
+  let [score, setScore] = useState(0);
+
+  const createMoleHill = () => {
+    let hills = [];
+    for (let i = 0; i < 9; i++) {
+      hills.push(<MoleContainer key={i} setScore={setScore} score={score} />);
+    }
+    // Return background image and hills.
+    return (
+      <div
+        style={{
+          backgroundImage: `url("https://cdn.pixabay.com/photo/2017/04/04/23/54/rush-2203494_960_720.jpg")`,
+        }}
+      >
+        {hills}
+      </div>
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>React-a-Mole!</h1>
+      <p>{score}</p>
+      {createMoleHill()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mole.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import moleImg from "./mole.png";
+
+const Mole = (props) => {
+  useEffect(() => {
+    let randSeconds = Math.ceil(Math.random() * 50000);
+    let timer = setTimeout(() => {
+      props.toggle(false);
+    }, randSeconds);
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <div>
+      <img
+        style={{ width: "10vw" }}
+        src={moleImg}
+        onClick={props.handleClick}
+        alt="Mole"
+      />
+    </div>
+  );
+};
+
+export default Mole;
